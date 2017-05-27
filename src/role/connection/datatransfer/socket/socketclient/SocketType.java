55f8caa5a16671dbd41bdb1ef2cda7a1c079448e
VictorSu000 <287764747@qq.com>
2017-05-27 14:48:32 +0800

    add a class to save constants for type of sockets

---
+++ src/role/connection/datatransfer/socket/socketclient/SocketType.java
@@ -0,0 +1,11 @@
+package role.connection.datatransfer.socket.socketclient;
+
+/**
+ * Created by Victor on 2017/5/24.<br>
+ * <br>
+ * The constants of type of the sockets.
+ */
+public class SocketType {
+    static public int READ = 0;
+    static public int WRITE = 1;
+}
